12db5fff5e37a004339a4565c359a816ff93f99e
Leonardo Medina <leo_medina14@hotmail.com>
2025-03-18 01:35:02 -0500

    add/update proyect eureka server

--- eurekaserver/src/main/java/com/leodev/eurekaserver/config/EurekaConfiguration.java
+++ eurekaserver/src/main/java/com/leodev/eurekaserver/config/EurekaConfiguration.java
@@ -1,4 +1,36 @@
 package com.leodev.eurekaserver.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
+@ConfigurationProperties(prefix = "eurekaserver")
 public class EurekaConfiguration {
+
+    private Map<String, String> instance;
+    private Map<String, String> client;
+
+    public Map<String, String> getInstance() {
+        return instance;
+    }
+
+    public void setInstance(Map<String, String> instance) {
+        this.instance = instance;
+    }
+
+    public Map<String, String> getClient() {
+        return client;
+    }
+
+    public void setClient(Map<String, String> client) {
+        this.client = client;
+    }
+
+    public EurekaConfiguration(Map<String, String> instance, Map<String, String> client) {
+        this.instance = instance;
+        this.client = client;
+    }
+    public EurekaConfiguration() {}
 }
--- eurekaserver/src/main/java/com/leodev/eurekaserver/controller/EurekaServerController.java
+++ eurekaserver/src/main/java/com/leodev/eurekaserver/controller/EurekaServerController.java
@@ -1,4 +1,26 @@
 package com.leodev.eurekaserver.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.leodev.eurekaserver.config.EurekaConfiguration;
+import com.leodev.eurekaserver.model.EurekaServerProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EurekaServerController {
+
+    @Autowired
+    private EurekaConfiguration eurekaConfiguration;
+
+    @GetMapping("/eureka/read/properties")
+    public String getProperties() throws JsonProcessingException {
+        ObjectWriter owj = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        EurekaServerProperties eurekaServerProperties = new EurekaServerProperties();
+        eurekaServerProperties.setInstance(eurekaConfiguration.getInstance());
+        eurekaServerProperties.setClient(eurekaConfiguration.getClient());
+        return owj.writeValueAsString(eurekaServerProperties);
+    }
 }
